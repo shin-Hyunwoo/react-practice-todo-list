45d13092dfae2da0c244cfb0524f43fc41102aac
shin-Hyunwoo <interactiveshin@gmail.com>
2026-06-29 22:20:44 +0900

    ✨ chore: add temporary mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import TodoList from './components/TodoList';
 
+const tempData = [
+  { id: 1, content: '책 50페이지 읽기' },
+  { id: 2, content: '오후 5시 PT 수업 출석' },
+  { id: 3, content: '저녁 8시 리액트 학습하기' },
+];
+
 function App() {
   return (
     <>
