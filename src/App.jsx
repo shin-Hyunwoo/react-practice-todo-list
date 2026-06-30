56bbb8d37303e8de2beb5e5e0800dbdc8874b34b
shin-Hyunwoo <interactiveshin@gmail.com>
2026-06-30 14:39:31 +0900

    ✨ feat: get input data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const onChangeInput = (event) => {
+    setTodo(event.target.value);
+  };
 
   return (
     <>
       <h1>리액트로 만드는 투두리스트</h1>
       <div>
-        <input />
+        <input value={todo} onChange={onChangeInput} />
         <button>추가</button>
       </div>
       <TodoList todos={todos} />
